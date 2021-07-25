4d1991aba4eb4678abea09ad2e7fb7e81bd895c3
Anmol Agrawal <anmol.ag53@gmail.com>
2021-07-25 20:48:40 +0530

    Added Copyright component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Copyright from "./Components/Copyright";
 import Dashboard from "./Containers/Dashboard";
 
 function App() {
     return (
         <div className="App">
             <Dashboard />
+            <Copyright
+                appName="BMI Tracker"
+                developer="Anmol Agrawal"
+                developerUrl="https://anmolagrawal.tech/"
+            />
         </div>
     );
 }
